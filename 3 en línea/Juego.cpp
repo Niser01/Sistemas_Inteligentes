4abcc7a7ee3e3d96fd5225919b8aeebd97bfaba0
Sergio Nicolás <shaldaneg@unal.edu.co>
2023-02-09 13:13:11 -0500

    validaciones

--- 3 en línea/Juego.cpp
+++ 3 en línea/Juego.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <list>
+#include<stdlib.h>
 using namespace std;
 
 
@@ -48,18 +49,6 @@ void print_first_board(){
     print_board(f_tablero);
 }
 
-list<int> empty(char tablero[9]){
-    list<int> posiciones;
-    for (int i = 0; i < 9; i++)
-    {        
-        if (tablero[i]==' ')
-        {
-            posiciones.push_back(i);
-        }              
-    }
-    return posiciones;
-}
-
 int pos_juego(){
     int pos;
      while (true)
@@ -78,33 +67,66 @@ int pos_juego(){
 char *tablero_juego(char tablero[9],char letra_jugador,int pos_oponente){
     if (tablero[pos_oponente-1]==' '){
         tablero[pos_oponente-1]=letra_jugador;
+        
     }else{
         cout<< "La posicion ya se encuentra ocupada"<<endl;
     }
     return tablero;
 }
 
-void juego_maquina(char tablero[9],char letra_jugador){
-    char letra_máquina;
+char juego_maquina(char tablero[9],char letra_jugador){
+    char letra_maquina;
     if (letra_jugador == 'X' || letra_jugador =='x')
     {
-        letra_máquina='O';
+        letra_maquina='O';
         
     }else{
-        letra_máquina='X';
+        letra_maquina='X';
     }
-
-    
-
+    return letra_maquina;
 }
+
+char *jugadas_victoria(char tablero[9], int contador_jugadas, char letra_jugador){
+    srand(time(NULL));
+    char letra = juego_maquina(tablero,letra_jugador);
+    int victoria[9][3]={{0, 1, 2},{3, 4, 5},{6, 7, 8},{0, 3, 6},{1, 4, 7},{2, 5, 8},{0, 4, 8},{2, 4, 6}};
+    int pos_juego;
+
+    if(contador_jugadas<2){
+        pos_juego = 1+rand()%9;
+        tablero= tablero_juego(tablero,letra,pos_juego);
+    }else{
+        if(tablero[0] == letra_jugador){
+            if(tablero[1]==letra_jugador){
+                tablero= tablero_juego(tablero,letra,2);
+            }else if (tablero[2]==letra_jugador)
+            {
+                tablero= tablero_juego(tablero,letra,1);
+            }
+            
+            
+        }
+        
+    }
+}
+
+
+
 
 
 int main(){
     char letra_jugador = incializacion();
     char tablero_blanco[9]={' ',' ',' ',' ',' ',' ',' ',' ',' '};
+    int contador_jugadas=0;
     print_first_board();
     int pos_oponente = pos_juego();
     char *tablero = tablero_juego(tablero_blanco,letra_jugador,pos_oponente);
+    print_board(tablero);
+    cout<<"--------------"<<endl;
+    tablero = jugadas_victoria(tablero,contador_jugadas,letra_jugador);
+    cout<<"--------------"<<endl;
+    print_board(tablero);
+
 
 
 
