ad5f2da03c3840353aa0023f4e61f204ee3f7ac9
Daniel Zuñiga <33994323+dzunigae@users.noreply.github.com>
2023-02-09 14:19:38 -0500

    Programa completo, falta mejorar la toma de decisión

--- 3 en línea/Daniel/TicTacToe.py
+++ 3 en línea/Daniel/TicTacToe.py
@@ -1,10 +1,12 @@
 import random
 
 class TicTacToe:
+    #Definición inicial del tablero y el símbolo que representa al jugador
     def __init__(self):
         self.board = [' ' for x in range(9)]
         self.player = 'X'
 
+    #Función para imprimir el tablero en consola
     def display_board(self):
         print(f' {self.board[0]} | {self.board[1]} | {self.board[2]} ')
         print('---+---+---')
@@ -12,27 +14,33 @@ class TicTacToe:
         print('---+---+---')
         print(f' {self.board[6]} | {self.board[7]} | {self.board[8]} ')
 
+    #Función que le permite al jugador hacer una movida
     def player_move(self):
-        move = int(input(f"Enter your move (1-9): ")) - 1
+        move = int(input("Realiza tu movimiento (1-9): ")) - 1
         if self.board[move] == ' ':
             self.board[move] = self.player
             self.display_board()
         else:
-            print("The position is already occupied. Try again.")
+            print("La posición ya está ocupada.")
             self.player_move()
 
+    #Función que decide el movimiento de la máquina
     def computer_move(self):
-        available_moves = [i for i, x in enumerate(self.board) if x == ' ']
+        available_moves = [i for (i, x) in enumerate(self.board) if x == ' ']
+
+        #Hay que cambiar este random por algo más elaborado
         move = random.choice(available_moves)
+
         self.board[move] = 'O'
-        print("The computer moves:")
+        print("El ordenador movió:")
         self.display_board()
 
+    #Ver si ya hay un ganador
     def winner(self):
         winning_combinations = [
-            [0, 1, 2], [3, 4, 5], [6, 7, 8],  # rows
-            [0, 3, 6], [1, 4, 7], [2, 5, 8],  # columns
-            [0, 4, 8], [2, 4, 6]  # diagonals
+            [0, 1, 2], [3, 4, 5], [6, 7, 8],  # Filas
+            [0, 3, 6], [1, 4, 7], [2, 5, 8],  # Columnas
+            [0, 4, 8], [2, 4, 6]  # Diagonales
         ]
         for combination in winning_combinations:
             if self.board[combination[0]] == self.board[combination[1]] == self.board[combination[2]] and self.board[combination[0]] != ' ':
@@ -40,25 +48,24 @@ class TicTacToe:
 
         return None
 
+    #Fin del juego
     def game_over(self):
         if self.winner() or ' ' not in self.board:
             return True
         return False
 
+    #Función Main de la clase
     def play(self):
-        print("Welcome to Tic Tac Toe!")
-        print("You will be playing against the computer.")
-        print("You will be X's and the computer will be O's.")
 
-        first_player = input("Would you like to start? (y/n) ")
+        print("Bienvenido al juego")
+        print("Tu serás las 'X' y el computador el '0'")
+        first_player = input("¿Deseas ser el primero en jugar? (y/n) ")
+        
         while first_player != 'y' and first_player != 'n':
-            first_player = input("Invalid input. Would you like to start? (y/n) ")
+            first_player = input("Simbolo incorrecto, intentalo nuevamente.")
 
         self.display_board()
         while not self.game_over():
-            if self.winner():
-                print(f"{self.winner()} won the game.")
-                break
             if first_player == 'y':
                 self.player_move()
                 first_player = 'n'
@@ -66,13 +73,11 @@ class TicTacToe:
                 self.computer_move()
                 first_player = 'y'
             if self.winner() == 'X':
-                print("You won the game!")
-                break
+                print("¡Ganaste!")
             elif self.winner() == 'O':
-                print("The computer won the game.")
-                break
-        if ' ' not in self.board:
-            print("It's a draw!")
+                print("El computador te ganó")
+            elif ' ' not in self.board:
+                print("Empate")
 
 game = TicTacToe()
 game.play()
