662c37d4f5c23e439183e11e9d5f503f326bd02c
Sergio Nicolás <shaldaneg@unal.edu.co>
2023-02-08 22:05:43 -0500

    up

--- 3 en línea/Juego.cpp
+++ 3 en línea/Juego.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <list>
 using namespace std;
 
 void print_board(char tablero[3][3]){   
@@ -13,8 +14,32 @@ void print_board(char tablero[3][3]){
 }
 
 
-void empty(char tablero[3][3]){
+list<int> empty(char tablero[3][3]){
+    list<int> posiciones;
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (tablero[i][j]=='*')
+            {
+                posiciones.push_back(i);
+                posiciones.push_back(j);
+            }
+            
+        }
+    }
 
+/*
+
+    list<int> posiciones=empty(tablero);
+    list<int>::iterator it_pos=posiciones.begin();
+    while (it_pos!=posiciones.end())
+    {
+        cout<<*it_pos++<<endl;
+    }
+*/
+
+    return posiciones;
 }
 
 
@@ -22,11 +47,11 @@ void empty(char tablero[3][3]){
 
 int main(){
 
-
     char tablero[3][3]={{'*','*','*'},{'*','*','*'},{'*','*','*'}};
     print_board(tablero);
 
-
+    
+    
 
 
     return 0;
