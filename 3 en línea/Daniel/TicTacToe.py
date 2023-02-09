87375a57798c4ddb35761ee560fcfee33216ac88
Daniel Zuñiga <33994323+dzunigae@users.noreply.github.com>
2023-02-09 08:42:37 -0500

    Corriendo sin optimizar

--- 3 en línea/Daniel/TicTacToe.py
+++ 3 en línea/Daniel/TicTacToe.py
@@ -1,6 +1,9 @@
+import random
+
 class TicTacToe:
     def __init__(self):
-        self.board = [' '] * 9
+        self.board = [' ' for x in range(9)]
+        self.player = 'X'
 
     def display_board(self):
         print(f' {self.board[0]} | {self.board[1]} | {self.board[2]} ')
@@ -9,95 +12,67 @@ class TicTacToe:
         print('---+---+---')
         print(f' {self.board[6]} | {self.board[7]} | {self.board[8]} ')
 
+    def player_move(self):
+        move = int(input(f"Enter your move (1-9): ")) - 1
+        if self.board[move] == ' ':
+            self.board[move] = self.player
+            self.display_board()
+        else:
+            print("The position is already occupied. Try again.")
+            self.player_move()
+
+    def computer_move(self):
+        available_moves = [i for i, x in enumerate(self.board) if x == ' ']
+        move = random.choice(available_moves)
+        self.board[move] = 'O'
+        print("The computer moves:")
+        self.display_board()
+
     def winner(self):
-        winning_positions = [
+        winning_combinations = [
             [0, 1, 2], [3, 4, 5], [6, 7, 8],  # rows
             [0, 3, 6], [1, 4, 7], [2, 5, 8],  # columns
             [0, 4, 8], [2, 4, 6]  # diagonals
         ]
-        for pos in winning_positions:
-            if self.board[pos[0]] == self.board[pos[1]] == self.board[pos[2]] and self.board[pos[0]] != ' ':
-                return self.board[pos[0]]
+        for combination in winning_combinations:
+            if self.board[combination[0]] == self.board[combination[1]] == self.board[combination[2]] and self.board[combination[0]] != ' ':
+                return self.board[combination[0]]
+
         return None
 
-    def minimax(self, player):
-        if self.winner() == 'X':
-            return -1
-        if self.winner() == 'O':
-            return 1
-        if ' ' not in self.board:
-            return 0
+    def game_over(self):
+        if self.winner() or ' ' not in self.board:
+            return True
+        return False
 
-        best_val = None
-        for i in range(9):
-            if self.board[i] == ' ':
-                self.board[i] = player
-                val = self.minimax(self.get_other_player(player))
-                self.board[i] = ' '
-                if player == 'O':
-                    if best_val is None or val > best_val:
-                        best_val = val
-                else:
-                    if best_val is None or val < best_val:
-                        best_val = val
-        return best_val
-
-    def get_other_player(self, player):
-        if player == 'X':
-            return 'O'
-        return 'X'
-
-    def get_best_move(self, player):
-        best_val = None
-        best_move = None
-        for i in range(9):
-            if self.board[i] == ' ':
-                self.board[i] = player
-                val = self.minimax(self.get_other_player(player))
-                self.board[i] = ' '
-                if player == 'O':
-                    if best_val is None or val > best_val:
-                        best_val = val
-                        best_move = i               
-                else:
-                    if best_val is None or val < best_val:
-                        best_val = val
-                        best_move = i
-        return best_move
-
-    def play_game(self):
+    def play(self):
         print("Welcome to Tic Tac Toe!")
         print("You will be playing against the computer.")
         print("You will be X's and the computer will be O's.")
+
+        first_player = input("Would you like to start? (y/n) ")
+        while first_player != 'y' and first_player != 'n':
+            first_player = input("Invalid input. Would you like to start? (y/n) ")
+
         self.display_board()
-        while True:
-            player = 'X'
-            player_move = int(input("Enter your move (1-9): ")) - 1
-            self.board[player_move] = player
+        while not self.game_over():
+            if self.winner():
+                print(f"{self.winner()} won the game.")
+                break
+            if first_player == 'y':
+                self.player_move()
+                first_player = 'n'
+            else:
+                self.computer_move()
+                first_player = 'y'
             if self.winner() == 'X':
-                print("You won!")
+                print("You won the game!")
                 break
             elif self.winner() == 'O':
-                print("The computer won!")
+                print("The computer won the game.")
                 break
-            elif ' ' not in self.board:
-                print("It's a draw.")
-                break
-
-            player = 'O'
-            computer_move = self.get_best_move(player)
-            self.board[computer_move] = player
-            if self.winner() == 'X':
-                print("You won!")
-                break
-            elif self.winner() == 'O':
-                print("The computer won!")
-                break
-            elif ' ' not in self.board:
-                print("It's a draw.")
-                break
-
-            self.display_board()
+        if ' ' not in self.board:
+            print("It's a draw!")
 
 game = TicTacToe()
-game.play_game()
+game.play()
