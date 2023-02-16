2ffd1026e80195ff0978880dd8ef353ffad33303
Sergio Nicolás <shaldaneg@unal.edu.co>
2023-02-16 17:19:21 -0500

    Creacion de clase agente

--- 3 en línea/3_en_linea.cpp
+++ 3 en línea/3_en_linea.cpp
@@ -1,2 +1,37 @@
 #include <iostream>
 using namespace std;
+
+
+/*Clase Agente-------------------------------------------------------*/
+class Agente{
+    private:
+        char tablero[9];
+    public:
+        Agente(char[9]);
+        int jugada();
+};
+
+Agente::Agente(char _tablero[9]){
+    tablero[9] = _tablero[9];
+}
+
+int Agente::jugada(){
+
+
+    return 0;
+}
+
+/*Clase Entorno--------------------------------------------------------*/
+
+
+
+
+
+
+
+int main(){
+
+
+
+    return 0;
+}
